edcbc82f32b1b4e230222cbbc7c4864cc38e60dd
artem <unbe@users.noreply.github.com>
2015-06-21 12:27:23 +0200

    Support battery handlers.

--- src/megaface.c
+++ src/megaface.c
@@ -4,18 +4,39 @@
 static Window *s_main_window;
 
 typedef struct _LayerInfo {
-  TextLayer *textLayer;
   Layer *layer;
   GRect frame;
-  void (*get_text)(struct tm *t, char *buffer);
   TimeUnits changes_on;
+  GColor8 color;
+  void (*handle_tick)(struct _LayerInfo* layer, struct tm *tick_time, TimeUnits units_changed);
+  void (*handle_battery)(struct _LayerInfo* layer, BatteryChargeState charge);
+
+  // Text layers
+  TextLayer *textLayer;
+  void (*get_text)(struct tm *t, char *buffer);
   char buffer[BUFFER_SIZE];
   const char* font_key;
-  GColor8 color;
 } LayerInfo;
+
+static void text_tick_handler(LayerInfo* layer, struct tm *tick_time, TimeUnits units_changed) {
+  layer->get_text(tick_time, layer->buffer);
+  text_layer_set_text(layer->textLayer, layer->buffer);
+}
+
+static void text_battery_handler(LayerInfo* layer, BatteryChargeState charge) {
+  static char buffer[20];
+  buffer[0] = charge.is_charging ? 'c':' ';
+  buffer[1] = charge.is_plugged ? 'p' : ' ';
+  buffer[2] = (charge.charge_percent / 100) + '0';
+  buffer[3] = ((charge.charge_percent / 10) % 10) + '0';
+  buffer[4] = (charge.charge_percent % 10) + '0';
+  buffer[5] = 0;
+  text_layer_set_text(layer->textLayer, buffer);
+}
 
 static LayerInfo layers[] = {
  { 
+    .handle_tick = &text_tick_handler,
     .get_text = &fuzzy_hours_to_words,
     .changes_on = HOUR_UNIT,
     .frame = {{0, -8}, {144, 50}},
@@ -23,6 +44,7 @@ static LayerInfo layers[] = {
     .color = { .argb = GColorYellowARGB8 },
  },
  {  
+    .handle_tick = &text_tick_handler,
     .get_text = &fuzzy_minutes_to_words,
     .changes_on = MINUTE_UNIT,
     .frame = {{0, 32}, {144, 50}},
@@ -30,6 +52,7 @@ static LayerInfo layers[] = {
     .color = { .argb = GColorWhiteARGB8 },
   },
  { 
+    .handle_tick = &text_tick_handler,
     .get_text = &fuzzy_sminutes_to_words,
     .changes_on = MINUTE_UNIT,
     .frame = {{0, 72}, {144, 50}},
@@ -37,40 +60,49 @@ static LayerInfo layers[] = {
     .color = { .argb = GColorWhiteARGB8 },
  },
  { 
+    .handle_tick = &text_tick_handler,
     .get_text = &fuzzy_dates_to_words,
     .changes_on = DAY_UNIT | MONTH_UNIT | YEAR_UNIT,
     .frame = {{0, 126}, {144, 36}},
     .font_key = FONT_KEY_GOTHIC_28_BOLD,
     .color = { .argb = GColorWhiteARGB8 },
  },
+ { 
+    .handle_battery = &text_battery_handler,
+    .frame = {{100, 126}, {144, 36}},
+    .font_key = FONT_KEY_GOTHIC_28_BOLD,
+    .color = { .argb = GColorWhiteARGB8 },
+ },
 };
+
 static int num_layers = sizeof(layers)/sizeof(layers[0]);
 
-static void update_time(struct tm *tick_time, TimeUnits units_changed) {
-  for (int i = 0; i < num_layers; i++) {
-    LayerInfo* layer = &(layers[i]);
-    if ((units_changed & layer->changes_on) == 0) {
-      continue;
-    }
-    layer->get_text(tick_time, layer->buffer);
-    text_layer_set_text(layer->textLayer, layer->buffer);
-  }
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+ for (int i = 0; i < num_layers; i++) {
+   LayerInfo* layer = &(layers[i]);
+   if (layer->handle_tick && (units_changed & layer->changes_on)) {
+     layer->handle_tick(layer, tick_time, units_changed);
+   }
+ }
 }
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time(tick_time, units_changed);
+static void battery_handler(BatteryChargeState charge) {
+ for (int i = 0; i < num_layers; i++) {
+   LayerInfo* layer = &(layers[i]);
+   if (layer->handle_battery) {
+     layer->handle_battery(layer, charge);
+   }
+ }
 }
 
 static Layer* init_layer(LayerInfo* layer) {
   TextLayer *textLayer = layer->textLayer = text_layer_create(layer->frame);
   text_layer_set_background_color(textLayer, GColorClear);
-  text_layer_set_text_color(textLayer, layer->color);
-
-  // Improve the layout to be more like a watchface
-  text_layer_set_font(textLayer, fonts_get_system_font(layer->font_key));
   text_layer_set_text_alignment(textLayer, GTextAlignmentLeft);
 
-  // Add it as a child layer to the Window's root layer
+  text_layer_set_text_color(textLayer, layer->color);
+  text_layer_set_font(textLayer, fonts_get_system_font(layer->font_key));
+
   return text_layer_get_layer(textLayer);
 }
 
@@ -81,7 +113,9 @@ static void main_window_load(Window *window) {
   }
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
-  update_time(tick_time, SECOND_UNIT | MINUTE_UNIT | HOUR_UNIT | DAY_UNIT | MONTH_UNIT | YEAR_UNIT);
+  tick_handler(tick_time, SECOND_UNIT | MINUTE_UNIT | HOUR_UNIT |
+                          DAY_UNIT | MONTH_UNIT | YEAR_UNIT);
+  battery_handler(battery_state_service_peek());
 }
 
 static void main_window_unload(Window *window) {
@@ -103,6 +137,7 @@ static void init(void) {
 
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  battery_state_service_subscribe(battery_handler);
 }
 
 static void deinit(void) {
